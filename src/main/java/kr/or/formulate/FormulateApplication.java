022e13bf1579c3984200e13271840f93642b1212
Formulate <nervers1@gmail.com>
2020-04-11 20:58:42 +0900

    the First Commit.

---
+++ src/main/java/kr/or/formulate/FormulateApplication.java
@@ -0,0 +1,13 @@
+package kr.or.formulate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulateApplication.class, args);
+	}
+
+}
