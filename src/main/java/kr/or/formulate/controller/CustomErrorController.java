c4f9111bad7dd823886e7fb8ce5196bda4052166
Formulate <nervers1@gmail.com>
2020-04-13 00:41:23 +0900

    1. ErrorController 구현 : 익스11에선 왜 안되지? 컨텐트 타입문제라는데, 정확한 답이 구글링해도 아직 없는 듯 ... 우선은 여기까지.

--- src/main/java/kr/or/formulate/controller/CustomErrorController.java
+++ src/main/java/kr/or/formulate/controller/CustomErrorController.java
@@ -3,6 +3,7 @@ package kr.or.formulate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,31 +15,32 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @Controller
 public class CustomErrorController implements ErrorController {
+    private String VIEW_PATH = "errorPages/";
 
-    @RequestMapping(value = "/error")
+    @RequestMapping(value = "/error", produces=MediaType.TEXT_PLAIN_VALUE)
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         log.info("CustomErrorController -> status : {}", status);
         if (status != null) {
             int statusCode = Integer.valueOf(status.toString());
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "forward:/customError404";
+                return VIEW_PATH + "404";
             }
             if (statusCode == HttpStatus.FORBIDDEN.value()) {
-                return "forward:/customError500";
+                return VIEW_PATH + "500";
             }
         }
         return "error";
     }
 
-    @GetMapping(path = "/customError404")
+    @GetMapping(path = "/404", produces=MediaType.TEXT_PLAIN_VALUE)
     public String notFound(Model model) {
-        return "/errorPages/404";
+        return "errorPages/404";
     }
 
-    @GetMapping(path = "/customError500")
+    @GetMapping(path = "/500", produces=MediaType.TEXT_PLAIN_VALUE)
     public String internalServerError(Model model) {
-        return "/errorPages/500";
+        return "errorPages/500";
     }
 
     @Override
