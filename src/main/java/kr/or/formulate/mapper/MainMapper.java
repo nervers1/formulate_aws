f13ff01b07adbbb22e1b88b233662f68a8707e4e
Formulate <nervers1@gmail.com>
2020-07-28 22:23:28 +0900

    MyBatis 땜에 @Repository를 추가했는데, 이런건 좀  아닌 것 같다.

--- src/main/java/kr/or/formulate/mapper/MainMapper.java
+++ src/main/java/kr/or/formulate/mapper/MainMapper.java
@@ -1,9 +1,11 @@
 package kr.or.formulate.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 
+@Repository
 @Mapper
 public interface MainMapper {
     LocalDateTime current();
